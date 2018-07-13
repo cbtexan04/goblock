500fbcb7c727fe356b345a4d93bd714a10ccc46b
William Taylor <william.taylor@fireeye.com>
2018-07-13 15:48:03 -0400

    Create initial functions for handling reading and writing blocks

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Block struct {
@@ -58,4 +63,35 @@ func (b *Block) IsValidNextBlock(nextBlock *Block) bool {
 	}
 
 	return true
+}
+
+func displayChain(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+func writeBlock(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+func run() error {
+	mr := mux.NewRouter()
+	mr.HandleFunc("/", displayChain).Methods("GET")
+	mr.HandleFunc("/", writeBlock).Methods("POST")
+
+	port := os.Getenv("ADDR")
+
+	s := &http.Server{
+		Addr:           ":" + port,
+		Handler:        mr,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Printf("Listening on ", port)
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
 }
